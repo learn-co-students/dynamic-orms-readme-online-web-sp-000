e89a4cb24ee345cb4c5dabbecd9307f05d6570b1
andy-aguilar <aaguil3@gmail.com>
2020-12-22 05:17:04 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,7 @@ class Song
       self.send("#{property}=", value)
     end
   end
+  
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
